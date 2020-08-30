b81244695dd5d634ff5f05d41514d5ab128e558b
CoffeeCode <laurenz.noffke@outlook.de>
2020-08-30 13:27:48 +0200

    .

--- test/test.js
+++ test/test.js
@@ -7,7 +7,9 @@ const KeyvCosmos = require("../dist/index");
 const connectionString = process.env.connectionString || "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 const databaseId = process.env.databaseId || "KeyvTest";
 const containerId = process.env.containerId || "KeyvTestContainer";
-if (process.env.isLocal == true)
+const isLocal = process.env.connectionString == undefined;
+
+if (isLocal == true)
 	process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 // keyvOfficialTests(test, Keyv, connectionString, "");
